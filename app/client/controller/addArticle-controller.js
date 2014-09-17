ff4d1cf5415e3be5cd1de177dd9c8201bb5376a7
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2014-09-17 16:54:29 +0200

    some angular progress

--- app/client/controller/addArticle-controller.js
+++ app/client/controller/addArticle-controller.js
@@ -5,7 +5,31 @@
 
 var addArticleController = angular.module('addArticleController', ['service.article', 'service.behaviour']);
 
-addArticleController.controller('addArticle', ['$scope', '$location', 'article','behaviour',
+addArticleController.directive('myImage', [ '$timeout',
+    function ( $timeout) {
+        return function (scope, elm, attrs) {
+            scope.$parent.ImgWidth = elm[0].offsetWidth;
+            elm.on('error', function () {
+                scope.$apply(function(){
+                    scope.urlIsImage = false;
+                });
+                console.log('error during loading '+scope.submitArticle.url);
+            });
+            elm.on('load', function () {
+                scope.$apply(function(){
+                   scope.urlIsImage = true;
+                });
+                console.log('img loaded '+scope.submitArticle.url);
+//                scope.$apply(function () {
+//                    scope.ImgWidth = elm[0].offsetWidth;
+//                });
+            });
+        };
+    }]);
+
+addArticleController.directive
+
+addArticleController.controller('addArticle', ['$scope', '$location', 'article', 'behaviour',
     function ($scope, $location, article, behaviour) {
         $scope.postArticle = function ($event, $form) {
             $event.preventDefault();
@@ -15,7 +39,17 @@ addArticleController.controller('addArticle', ['$scope', '$location', 'article',
             }
         };
 
+        $scope.urlIsImage = false;
+        $scope.sayHello = function () {
+            console.log('say hello...');
+        }
+        $scope.handleImgUrlError = function () {
+            console.log('error during loading img');
+        };
+        // the ability to auto resize the description textarea vertically.
         $scope.autoResizeTextarea = behaviour.autoResizeTextarea;
+
+
 //        articleServices.getArticles().success(function(data, status, headers, config) {
 //            $scope.articles = data;
 //            console.log('success with get articles!');
