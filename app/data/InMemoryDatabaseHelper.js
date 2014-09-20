3601eaf6a787e9ccc96b17d564cd035f8248475f
fade2g <holger.heymanns@gmail.com>
2014-09-20 21:48:37 +0200

    Working on InMemoryDatabase - including unit tests. Will try to mock lnk.services, but with more entities and modules

---
+++ app/data/InMemoryDatabaseHelper.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Holger on 20.09.2014.
+ * This module contains some helper functions for the in memory database
+ */
+
+var log4js = require('log4js');
+var logger = log4js.getLogger('data.InMemoryDatabaseHelper');
+
+var inMemoryDatabaseHelper = {};
+
+/**
+ * This function checks, if properties required are available
+ * @param dataArray The array containing data
+ * @param idProperty The name of the id property
+ * @return {boolean} True, if both proeprties are well defined
+ */
+var checkLoopProperties = function(dataArray, idProperty) {
+    if(dataArray === null || dataArray === undefined || idProperty === null || idProperty === undefined) {
+        logger.warn('Searching new ID with incomplete data, dataArray=' + dataArray + ', idProperty=' + idProperty);
+        return false;
+    }
+    return true;
+};
+
+/**
+ * This function generates a new ID for the article
+ * @param dataArray {object[]} array to search for an id
+ * @param idProperty {String} name of the property, that contains the id value
+ * @return {Number} return the new max value or -1 if no value could be found
+ */
+var getNewId = function(dataArray, idProperty) {
+    var maxValue = -1;
+    if (checkLoopProperties(dataArray, idProperty)) {
+        for (var i = 0, len = dataArray.length; i < len; i += 1) {
+            maxValue = (dataArray[i][idProperty] && dataArray[i][idProperty]) > maxValue ? dataArray[i][idProperty] : maxValue;
+        }
+        maxValue += 1;
+        logger.debug('New max value ' + maxValue);
+    }
+    return maxValue;
+};
+
+/**
+ * This function finds an element with a given id in an array
+ * @param dataArray {object[]}
+ * @param idProperty [String}
+ * @param idValue {Number}
+ * @return {number}
+ */
+var findPositionById = function(dataArray, idProperty, idValue) {
+    if(!checkLoopProperties(dataArray, idProperty)) {
+        return;
+    }
+    for (var i = 0, len = dataArray.length; i < len; i += 1) {
+        if(dataArray[i][idProperty] && dataArray[i][idProperty] === idValue) {
+            logger.debug('Found item with id ' + idValue + ' at position ' + i);
+            return i;
+        }
+    }
+    logger.info('Found no item with id ' + idValue);
+};
+
+inMemoryDatabaseHelper = {
+    findPositionById: findPositionById,
+    getNewId: getNewId
+}
+
+module.exports = inMemoryDatabaseHelper;
---
+++ test/spec/modules/data/InMemoryDatabaseHelper-spec.js
@@ -0,0 +1,63 @@
+var path = require('path');
+var app_constants = require(path.join(path.resolve(process.cwd()), 'app_constants'));
+var dbHelper = require(app_constants.packagedModule('data', 'InMemoryDatabaseHelper'));
+
+describe('Test InMemoryDatabaseHelper', function() {
+    var dataArray;
+
+    beforeEach(function () {
+        dataArray = [
+            {
+                id: 1,
+                value: 'unimportant'
+            },
+            {
+                id: 2,
+                value: 'dont need it'
+            }
+        ];
+    });
+    describe('Test getNewId', function () {
+        it('finds a new id', function () {
+            expect(dbHelper.getNewId(dataArray, 'id')).toBe(3);
+            dataArray.push({id: 17, value: 'go away'});
+            expect(dbHelper.getNewId(dataArray, 'id')).toBe(18);
+            dataArray.push({id: 4, value: 'go away'});
+            expect(dbHelper.getNewId(dataArray, 'id')).toBe(18);
+        });
+        it('returns -1 if properties ar wrong', function () {
+            expect(dbHelper.getNewId(null, 'id')).toBe(-1);
+            expect(dbHelper.getNewId(undefined, 'id')).toBe(-1);
+            expect(dbHelper.getNewId(dataArray, null)).toBe(-1);
+            expect(dbHelper.getNewId(dataArray, undefined)).toBe(-1);
+        })
+    });
+    describe('Test findPositionById', function() {
+        var cases = [{id: 1, expected: 0}, {id: 2, expected: 1}, {id: 4, expected: undefined}];
+        var counter;
+        var caseLength = cases.length;
+        var testCase;
+        function assertPosition(testCase) {
+            it('finds position for id ' + testCase.id + ' to be ' + testCase.expected, function() {
+                expect(dbHelper.findPositionById(dataArray, 'id', testCase.id)).toBe(testCase.expected);
+            });
+        }
+        for(counter = 0; counter < cases.length; counter++) {
+            assertPosition(cases[counter]);
+        }
+    })
+    describe('Test findPositionById with wrong properties', function() {
+        var cases = [{id: null, expected: undefined}, {id: undefined, expected: undefined}];
+        var counter;
+        var caseLength = cases.length;
+        var testCase;
+        function assertPosition(testCase) {
+            it('finds wrong position for id ' + testCase.id + ' to be ' + testCase.expected, function() {
+                expect(dbHelper.findPositionById(dataArray, 'id', testCase.id)).toBe(testCase.expected);
+            });
+        }
+        for(counter = 0; counter < cases.length; counter++) {
+            assertPosition(cases[counter]);
+        }
+    })
+});
