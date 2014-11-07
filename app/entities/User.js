5b40ce79f0743370b8b69f8a5bea7961503409d0
fade2g <holger.heymanns@gmail.com>
2014-11-07 21:50:37 +0100

    enhance user objectd

--- app/entities/User.js
+++ app/entities/User.js
@@ -9,16 +9,26 @@ var bcrypt = require('bcryptjs');
  * Create a new user object
  * @param {Number} id must come from the datasets
  * @param {String} userName user name
- * @param {String} password plaintext password, will be transfotrmed in to a encrytped password
+ * @param {String} name the name of the user
+ * @param {String} firstname the first name of the user
+ * @param {String} password (must be encrypted)
  * @param {boolean} active true or false, if the user is active or not
  * @constructor
  */
-function User(id, userName, password, active) {
+function User(id, userName, name, firstname, password, active) {
     this.id = id;
     this.userName= userName;
-    this.password = bcrypt.hashSync(password ? password : 'dummy', 8);
+    this.name = name;
+    this.firstname = firstname;
+    this.password = password;
     this.active = active;
 }
+
+User.prototype.encryptPassword = function(password) {
+    if (password !== undefined || password !== null) {
+        return bcrypt.hashSync(password, 8);
+    }
+};
 
 /**
  * This function updates the properties of the comment object with the given properties from the json object. If the property
@@ -28,6 +38,8 @@ function User(id, userName, password, active) {
 User.prototype.updateFromJsonObject = function(jsonObject) {
     this.id = jsonObject.id ? jsonObject.id : this.id;
     this.userName = jsonObject.userName ? jsonObject.userName : this.userName;
+    this.name = jsonObject.name ? jsonObject.name : this.name;
+    this.firstname = jsonObject.firstname ? jsonObject.firstname : this.firstname;
     this.password = jsonObject.password ? bcrypt.hashSync(jsonObject.password, 8) : this.password;
     this.active= jsonObject.active ? jsonObject.active : this.active;
 };
@@ -36,13 +48,25 @@ User.prototype.updateFromJsonString = function(jsonString) {
     this.updateFromJsonObject(JSON.parse(jsonString));
 };
 
-User.prototype.isAuthenticated = function(password) {
+/**
+ * This function compares a password with the stored, encrypted password
+ * @param {String} password the  password
+ * @param {boolean} encrypted If true, the provided password is assumed to be true
+ * @return {boolean} true, if the passwords match
+ */
+User.prototype.isAuthenticated = function(password, encrypted) {
+    if (!this.active) {
+        return false;
+    }
+    if (encrypted) {
+        return this.password === password;
+    }
     return bcrypt.compareSync(password, this.password);
 };
 
 User.prototype.clone = function() {
-    var user = new User(this.id, this.userName, '', this.active);
-    user.password = this.password; // note: the consctructor will encypt an already encrypted password
+    var user = new User(this.id, this.userName, this.name, this.firstname, '', this.active);
+    user.password = this.password; // note: the constructor will encypt an already encrypted password
     return user;
 };
 
--- test/spec/modules/entities/User-spec.js
+++ test/spec/modules/entities/User-spec.js
@@ -10,14 +10,18 @@ var UserClass = require(app_constants.packagedModule('entities', 'User.js'));
 describe('Test user entity', function () {
     describe('Test password functionality', function () {
         var testPassword = 'secret password';
-        var user = new UserClass.User(null, 'holger', testPassword, true);
+        var testEncryptedPassword = UserClass.User.prototype.encryptPassword(testPassword);
+
+        var user = new UserClass.User(null, 'holger', 'heymanns', 'holger', testEncryptedPassword, true);
 
         function assertPassword(user) {
             expect(user.isAuthenticated(testPassword)).to.be.true;
+            expect(user.isAuthenticated(testEncryptedPassword, true)).to.be.true;
             expect(user.isAuthenticated('wrong password')).to.be.false;
+            expect(user.isAuthenticated(user.encryptPassword('wrong password'), true)).to.be.false;
         }
 
-        it('validates the password', function () {
+        it('validates the unencrypted password', function () {
             assertPassword(user);
         });
         it('validated the password of a cloned user', function () {
@@ -26,10 +30,12 @@ describe('Test user entity', function () {
     });
     describe('Test JSON updates', function() {
         it('Updates all values from JSON', function() {
-            var user = new UserClass.User(123, 'Andy', 'top secret',false);
+            var user = new UserClass.User(123, 'andy', 'xyz', 'jjsh', 'top secret',false);
             var newUserObject = {
                 id: 444,
                 userName: 'Hoegger',
+                name: 'blubber',
+                firstname: 'andy',
                 password: 'even more secret',
                 active: true
             };
@@ -37,15 +43,20 @@ describe('Test user entity', function () {
             user.updateFromJsonString(newUserJson);
             expect(user.id).to.be.equal(newUserObject.id);
             expect(user.userName).to.be.equal(newUserObject.userName);
+            expect(user.name).to.be.equal(newUserObject.name);
+            expect(user.firstname).to.be.equal(newUserObject.firstname);
             expect(user.active).to.be.equal(newUserObject.active);
             expect(user.isAuthenticated(newUserObject.password)).to.be.true;
         });
         it('Does not update not provided values', function() {
             var originalPassword = 'top secret';
-            var user = new UserClass.User(123, 'Andy', originalPassword, false);
+            var originalEncryptedPassword = UserClass.User.prototype.encryptPassword(originalPassword);
+            var user = new UserClass.User(123, 'andy', 'Hoegger', 'Andy', originalEncryptedPassword, true);
             user.updateFromJsonObject({});  // no property that will be used
             expect(user.id).to.be.equal(user.id);
             expect(user.userName).to.be.equal(user.userName);
+            expect(user.name).to.be.equal(user.name);
+            expect(user.firstname).to.be.equal(user.firstname);
             expect(user.active).to.be.equal(user.active);
             expect(user.isAuthenticated(originalPassword)).to.be.true;
         })
