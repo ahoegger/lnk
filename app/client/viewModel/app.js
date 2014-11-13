a35e17351c2fdae4f890739115d89dda6e1b2499
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2014-11-13 22:18:54 +0100

    added authentication to routes client side

--- app/client/viewModel/app.js
+++ app/client/viewModel/app.js
@@ -14,8 +14,10 @@ var lnkApp = angular.module('lnkApp', [
     , 'loginController'
     , 'navigationController'
     , 'userController'
-    ,'angular-momentjs',
-    'service.tokenInterceptor'
+    , 'angular-momentjs'
+    , 'service.tokenInterceptor'
+    , 'service.user'
+
 ]);
 
 lnkApp.config(['$routeProvider',
@@ -23,42 +25,54 @@ lnkApp.config(['$routeProvider',
         $routeProvider.
             when('/articles', {
                 templateUrl: 'views/article-list.html',
-                controller: 'articleListController'
+                controller: 'articleListController',
+                access: { requiredLogin: false }
             }).
             when('/add',{
                 templateUrl: 'views/addArticle.html',
-                controller: 'addArticle'
+                controller: 'addArticle',
+                access: { requiredLogin: true }
+
             }).
             when('/article/:articleId', {
                 templateUrl: 'partials/phone-detail.html',
-                controller: 'PhoneDetailCtrl'
+                controller: 'PhoneDetailCtrl',
+                access: { requiredLogin: false }
             }).
             when('/login',{
                 templateUrl: 'views/login.html',
-                controller: 'loginController'
+                controller: 'loginController',
+                access: { requiredLogin: false }
             }).
             when('/user:id',{
                templateUrl: 'views/userUpdate.html',
-                controller: 'userController'
+                controller: 'userController',
+                access: { requiredLogin: true }
             }).
             otherwise({
                 redirectTo: '/articles'
             });
     }
 ])
-    .run(function($rootScope, $location) {
-    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
-        console.log('route to: '+next.templateUrl);
-        console.dir(next);
-//            console.log('route changed');
-//        if ($rootScope.loggedInUser == null) {
-//            // no logged user, redirect to /login
-//            if ( next.templateUrl === "partials/login.html") {
-//            } else {
-//                $location.path("/login");
-//            }
-//        }
-    })});
+    .run(function($rootScope, $location, userServiceState) {
+        $rootScope.$on("$routeChangeStart", function(event, nextRoute, currentRoute) {
+            if (nextRoute.access.requiredLogin && !userServiceState.user) {
+                $location.path("/login");
+            }
+        });
+//        $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+//        console.log('route to: '+next.templateUrl);
+//        console.dir(next);
+////            console.log('route changed');
+////        if ($rootScope.loggedInUser == null) {
+////            // no logged user, redirect to /login
+////            if ( next.templateUrl === "partials/login.html") {
+////            } else {
+////                $location.path("/login");
+////            }
+////        }
+//        })
+    });
 
 lnkApp.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.interceptors.push('tokenInterceptor');
