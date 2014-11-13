ee60c521e9382651cb8547effeb3124a7baa2f34
fade2g <holger.heymanns@gmail.com>
2014-11-13 18:02:57 +0100

    Fixed issue with updating user

--- app/client/services/user-service.js
+++ app/client/services/user-service.js
@@ -9,21 +9,29 @@ userService.factory('userService',['$http',
 
     var getUserInternal = function(id){
 
-        return $http({method: 'GET', url: '/api/user:'+id}).
+        return $http({method: 'GET', url: '/api/user/' + id}).
             error(function (data, status, headers, config) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
                 console.log('An error during data access')
             });
-    }
-        var updateUserInternal = function(user){
-            return $http({method: 'PUT', url: '/api/user:'+user.id}).
-                error(function (data, status, headers, config) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                    console.log('An error during data access')
-                });
-        }
+    };
+
+    var updateUserInternal = function(user){
+        return $http({
+                method: 'PUT',
+                url: '/api/user/' + user.id,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: JSON.stringify(user)
+            })
+            .error(function (data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                console.log('An error during data access')
+            });
+    };
 
     return{
         getUser : getUserInternal,
